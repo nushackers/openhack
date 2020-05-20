283abfef9a2a834e62a4bf0df6a98bb6fa014acc
Christopher Goh <chrisgzf@gmail.com>
2020-05-21 03:45:58 +0800

    Split writeup into multiple pages

---
+++ pages/submit.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Page from "../components/Page";
+import Text from "../components/Text";
+
+const Index = () => (
+  <Page>
+    <h2 className="text-xl font-black">Submission</h2>
+    <Text className="my-5">
+      We will be updating this site with the submission process soon. Stay tuned
+      and start contributing now!
+    </Text>
+  </Page>
+);
+
+export default Index;
