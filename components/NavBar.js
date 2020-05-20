9a927511d66c53e38ffe3efa7846e11b46a2df66
Christopher Goh <chrisgzf@gmail.com>
2020-05-21 05:03:36 +0800

    Add styling for smaller viewports for NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,20 +2,25 @@ import Link from "next/link";
 
 const nav = () => (
   <>
-    <div className="header fixed top-0 w-full bg-opacity-100 bg-indigo-500">
-      <nav className="mx-auto w-11/12 max-w-screen-md md:w-5/6 my-4 px-4 md:px-12 py-1">
+    <div className="header fixed top-0 w-full bg-opacity-100">
+      <nav className="mx-auto max-w-screen-md w-full md:w-5/6 text-center md:text-left my-3 md:my-4 px-0 md:px-12">
         <Link className="inline-block" href="/">
-          <span className="font-medium text-white mr-5">Home</span>
+          <span className="font-medium text-white mr-6">Home</span>
         </Link>
-        <Link className="inline-block m-5" href="rules">
-          <span className="text-white mr-5">Rules</span>
+        <Link className="inline-block" href="rules">
+          <span className="text-white mr-6">Rules</span>
         </Link>
-        <Link className="inline-block m-5" href="submit">
-          <span className="text-white mr-5">Submit</span>
+        <Link className="inline-block" href="start">
+          <span className="text-white mr-6">Start</span>
+        </Link>
+        <Link className="inline-block" href="submit">
+          <span className="text-white">Submit</span>
         </Link>
       </nav>
     </div>
-    <div className="inline-block w-full my-5">OpenHack by NUS Hackers</div>
+    <div className="inline-block w-full my-3 md:my-4">
+      OpenHack by NUS Hackers
+    </div>
   </>
 );
 
