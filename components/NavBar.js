ad68d2512888d338e9f494fb7b385fa255973428
Daryl Chan <dckj95@gmail.com>
2020-05-23 18:46:43 +0800

    refactor: Navbar and tailwind config

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,23 +1,14 @@
-import NavButton from "./NavButton";
+import { NavButton } from "./NavButton";
 
-const nav = () => {
+export const NavBar = () => {
   return (
-    <>
-      <div className="header fixed top-0 w-full bg-opacity-100">
-        <nav className="mx-auto max-w-screen-md w-full md:w-5/6 text-center md:text-left my-3 md:my-4 px-0 md:px-12">
-          <NavButton href="/">Home</NavButton>
-          <NavButton href="/rules">Rules</NavButton>
-          <NavButton href="/start">Start</NavButton>
-          <NavButton href="/submit" nomargin>
-            Submit
-          </NavButton>
-        </nav>
-      </div>
-      <div className="inline-block w-full my-3 md:my-4">
-        OpenHack by NUS Hackers
-      </div>
-    </>
+    <div className="header fixed w-full">
+      <nav className="w-full max-w-screen-md mx-auto my-3 px-0 md:px-12 text-center md:text-left">
+        <NavButton href="/">Home</NavButton>
+        <NavButton href="/rules">Rules</NavButton>
+        <NavButton href="/start">Start</NavButton>
+        <NavButton href="/submit">Submit</NavButton>
+      </nav>
+    </div>
   );
 };
-
-export default nav;
--- components/NavButton.js
+++ components/NavButton.js
@@ -2,24 +2,22 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import classnames from "classnames";
 
-const navBtn = ({ href, nomargin, children }) => {
-  const pathname = useRouter().pathname;
+export const NavButton = ({ href, children }) => {
+  const { pathname } = useRouter();
   return (
     <Link href={href}>
-      <a className="inline-block">
-        <span
-          className={classnames(
-            { "mr-6": !nomargin },
-            pathname == href
-              ? "font-bold text-white"
-              : "font-normal text-gray-300"
-          )}
-        >
-          {children}
-        </span>
+      <a
+        className={classnames(
+          "inline-block",
+          "mr-6",
+          "last:mr-0",
+          pathname === href
+            ? "font-bold text-white"
+            : "font-normal text-gray-300"
+        )}
+      >
+        {children}
       </a>
     </Link>
   );
 };
-
-export default navBtn;
