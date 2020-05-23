5e41b78a60c6d535d44ed3ce5b7c990ae5beadef
Daryl Chan <dckj95@gmail.com>
2020-05-23 19:51:20 +0800

    refactor: Submit

--- pages/submit.js
+++ pages/submit.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Page from "../components/Page";
-import Text from "../components/Text";
+
+import { Page } from "../components/Page";
+import { Text } from "../components/Text";
 
 const Submit = () => (
   <Page>
     <h2 className="text-xl font-black">Submission</h2>
-    <Text className="my-5">
+    <Text className="mt-5">
       We will be updating this site with the submission process soon. Stay tuned
       and start contributing now!
     </Text>
