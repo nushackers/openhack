5ab3d3d0c73a83709f2df020eaf3f1a8aed6bec4
Christopher Goh <chrisgzf@gmail.com>
2020-05-25 12:42:17 +0800

    Do some copy editing

--- pages/submit.js
+++ pages/submit.js
@@ -7,37 +7,39 @@ const Submit = () => (
   <Page>
     <h2 className="text-xl lg:text-2xl font-black">Submission</h2>
     <Text className="mt-5">
-      We will be using Github Issues to track submissions. Not only does it help
-      us maintain all the submissions centrally, but it also helps you maintain
-      permalinks to the work you did for OpenHack. You can also update your
-      submission incrementally instead of submitting it in one go, so we can
-      keep providing feedback on your PRs/project if necessary!
+      We will be using GitHub's Issue Tracker to track submissions. By doing so,
+      you can also update your submission incrementally (i.e. add PR after PR as
+      you complete them) instead of submitting it in one go, so that we can
+      quickly provide feedback on your PRs / project if necessary!
     </Text>
     <Text className="mt-5 mb-2">
       To make a submission, navigate to the{" "}
-      <a target="_blank" href="#" rel="noopener noreferrer">
+      <a
+        target="_blank"
+        href="https://github.com/nushackers/openhack-submissions/issues"
+        rel="noopener noreferrer"
+      >
         {" "}
-        OpenHack submission repository{" "}
+        OpenHack submission repository's issue tracker{" "}
       </a>
       and follow these instructions:
     </Text>
-    <ol className="mb-5">
-      <li className="ml-5 my-1">
-        Click at the <strong>Issues</strong> tab at the top, and then click the{" "}
-        <strong>New Issue</strong> button. Alternatively, you can click{" "}
-        <a href="#"></a>.
+    <ol className="mb-5 list-decimal">
+      <li className="ml-10 my-1">
+        Click on the <strong>New Issue</strong> button.
       </li>
-      <li className="ml-5 my-1">
-        Get started with the <strong>Submissions</strong> option.
+      <li className="ml-10 my-1">
+        Click on the <strong>Get started</strong> button on the submission
+        template.
       </li>
-      <li className="ml-5 my-1">
-        Submit your challenge details in the issue submission form. Change the
-        issue title to your name.
+      <li className="ml-10 my-1">Include your GitHub username in the title.</li>
+      <li className="ml-10 my-1">
+        Fill out the issue template with your submission details.
       </li>
-      <li className="ml-5 my-1">
+      <li className="ml-10 my-1">
         Note that we are using an issue template, so only edit the portions
         relevant to your submission. You can preview the submission form by
-        click on the <strong>Preview</strong> tab.
+        clicking on the <strong>Preview</strong> tab.
       </li>
     </ol>
     <Text className="mt-5 mb-2">
