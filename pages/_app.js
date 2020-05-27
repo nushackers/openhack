1f26b998657041c973b17f4f37af2995cc80e078
Christopher Goh <chrisgzf@gmail.com>
2020-05-28 01:27:49 +0800

    Add site analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "../styles.scss";
+import { useEffect } from "react";
+import Router from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
